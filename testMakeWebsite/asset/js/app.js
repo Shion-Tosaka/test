af27c1d48de80e2d6c208cf6a0a21178c30d3a6a
Shion <136096725+tasumaty@users.noreply.github.com>
2024-10-16 06:28:08 +0900

    Update app.js

--- testMakeWebsite/asset/js/app.js
+++ testMakeWebsite/asset/js/app.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles-js', 'asset/js/particles.json', function() {
+particlesJS.load('particles-js', 'test/MakeWebsite/asset/js/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
